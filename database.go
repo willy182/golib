ab6185a37fb4cdcbe420bb6bb95009ceedaa6783
jbernhardsch <julius.bernhard@gmail.com>
2017-09-25 18:27:39 +0700

    initialize commit and add logger and database component

---
+++ database.go
@@ -0,0 +1,131 @@
+package component
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+	_ "github.com/bmizerany/pq"
+	"github.com/jinzhu/gorm"
+	"strconv"
+)
+
+// dbWrite dbRead dbLogger: variable for database
+var (
+	dbWrite, dbRead *gorm.DB
+	dbLogger        *log.Logger
+	isDebug         bool
+)
+
+// DBLogFormatter database log formatter
+type DBLogFormatter struct {
+	EnableColor bool
+}
+
+// Format function to format the database log
+// entry log.Entry
+func (f *DBLogFormatter) Format(entry *log.Entry) ([]byte, error) {
+	keys := make([]string, 0, len(entry.Data))
+	for k := range entry.Data {
+		keys = append(keys, k)
+	}
+
+	b := &bytes.Buffer{}
+	if entry.Message != "" {
+		m := entry.Message
+		if f.EnableColor == false {
+			for _, v := range []string{
+				"\033[33m",
+				"\033[35m",
+				"\033[36;1m",
+				"\033[31;1m",
+				"\033[0m",
+			} {
+				m = strings.Replace(m, v, "", -1)
+			}
+		}
+		b.WriteString(m)
+	}
+	b.WriteByte('\n')
+	return b.Bytes(), nil
+}
+
+// InitDB function to initialize database log
+func InitDB() {
+	isDebug = false
+	if os.Getenv("DEBUG") == "1" {
+		isDebug = true
+	}
+	fmt.Println(fmt.Sprintf("debug: %v", isDebug))
+
+	if isDebug {
+		dbLogger = log.New()
+		lf := fmt.Sprintf("%s/logs/database.log", os.Getenv("STORAGE_DIR"))
+
+		f, err := os.OpenFile(lf, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			panic(err)
+		}
+		dbLogger.Formatter = &DBLogFormatter{EnableColor: false}
+		dbLogger.Out = f
+	}
+}
+
+// GetWriteDB function to get writing access to database
+func GetWriteDB() *gorm.DB {
+	if dbWrite == nil {
+		dbWrite = CreateDBConnection(fmt.Sprintf("host=%s user=%s "+
+			"password=%s dbname=%s sslmode=disable",
+			os.Getenv("DBW_HOST"), os.Getenv("DBW_USER"), os.Getenv("DBW_PASS"), os.Getenv("DBW_NAME")))
+	}
+	return dbWrite
+}
+
+// GetReadDB function to get reading access to database
+func GetReadDB() *gorm.DB {
+	if dbRead == nil {
+		//dbRead = CreateDBConnection(os.Getenv("DB_READ"))
+		dbRead = CreateDBConnection(fmt.Sprintf("host=%s user=%s "+
+			"password=%s dbname=%s sslmode=disable",
+			os.Getenv("DBR_HOST"), os.Getenv("DBR_USER"), os.Getenv("DBR_PASS"), os.Getenv("DBR_NAME")))
+
+	}
+	return dbRead
+}
+
+// CreateDBConnection function to create database connection
+func CreateDBConnection(descriptor string) *gorm.DB {
+	db, err := gorm.Open("postgres", descriptor)
+	if err != nil {
+		log.Fatal(err)
+		defer db.Close()
+	}
+
+	maxOpenCons, _ := strconv.Atoi(os.Getenv("DB_MAX_OPEN_CONS"))
+
+	// set max idle connection to zero
+	db.DB().SetMaxIdleConns(0)
+	db.DB().SetMaxOpenConns(maxOpenCons)
+
+	// set database log into file
+	if isDebug {
+		db.LogMode(true)
+		db.SetLogger(gorm.Logger{dbLogger})
+	}
+
+	return db
+}
+
+// CloseDb function for closing database connection
+func CloseDb() {
+	if dbRead != nil {
+		dbRead.Close()
+		dbRead = nil
+	}
+	if dbWrite != nil {
+		dbWrite.Close()
+		dbWrite = nil
+	}
+}
---
+++ logger.go
@@ -0,0 +1,151 @@
+package component
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"time"
+)
+
+// ResultLogger result logger interface
+type ResultLogger interface {
+	Store(c string, d []byte) string
+	Get(p string) string
+	RequestResponse(c string, dt string) string
+	LastError() error
+}
+
+// FileResultLogger file based storage
+type FileResultLogger struct {
+	baseDir   string
+	lastError error
+}
+
+// newFileResultLogger private function for creating log file
+// base string directory
+func newFileResultLogger(base string) *FileResultLogger {
+	this := new(FileResultLogger)
+	if err := this.createOrIgnore(base); err != nil {
+		this.lastError = err
+		return this
+	}
+	this.baseDir = base
+	return this
+}
+
+// LastError function for getting last error
+func (flo *FileResultLogger) LastError() error {
+	return flo.lastError
+}
+
+// createOrIgnore function for creating or ignoring error
+// p string
+func (flo *FileResultLogger) createOrIgnore(p string) error {
+	_, err := os.Stat(p)
+	if err != nil {
+		if os.IsNotExist(err) {
+			if err := os.MkdirAll(p, 0775); err != nil {
+				flo.lastError = err
+				return err
+			}
+		} else {
+			flo.lastError = err
+			return err
+		}
+	}
+
+	flo.lastError = nil
+	return nil
+}
+
+// GetFileName function to get file name
+// c string code name
+func (flo *FileResultLogger) GetFileName(c string) string {
+	t := time.Now()
+	u := strconv.Itoa(int(time.Now().Unix()))
+	f := fmt.Sprintf("%s_%s.%s", t.Format("150405"), u, c)
+	return flo.baseDir + "/" + c + "/" + f
+}
+
+// Get  function to get log
+// p string file name
+func (flo *FileResultLogger) Get(p string) string {
+	base := os.Getenv("STORAGE_DIR")
+	f := base + "/archive/" + p
+	data, err := ioutil.ReadFile(f)
+	if err != nil {
+		flo.lastError = err
+		return ""
+	}
+
+	flo.lastError = nil
+	return string(data)
+}
+
+// Store function to store log
+// c string code name
+// d []byte json data which is about to stored
+func (flo *FileResultLogger) Store(c string, d []byte) string {
+	fileName := flo.GetFileName(c)
+	if err := flo.createOrIgnore(flo.baseDir + "/" + c); err != nil {
+		flo.lastError = err
+		return fileName
+	}
+
+	flo.lastError = nil
+	if err := ioutil.WriteFile(fileName, d, 0775); err != nil {
+		flo.lastError = err
+	}
+
+	return fileName
+}
+
+// RequestResponse function for storing request and response into file
+// c string content
+// dt string date time
+func (flo *FileResultLogger) RequestResponse(c string, dt string) string {
+	t := time.Now()
+
+	// set the value of data
+	dir := os.Getenv("STORAGE_DIR") + "/logs/"
+	filename := fmt.Sprintf("%s%s.%s", dir, t.Format("20060102"), c)
+	val := fmt.Sprintf("%s : %s", t.Format("15:04:05"), dt)
+
+	if err := flo.createOrIgnore(dir); err == nil {
+		f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0775)
+		if err == nil {
+			if _, err = f.WriteString(val); err != nil {
+				panic(err)
+			}
+		}
+
+		defer f.Close()
+	}
+
+	return ""
+}
+
+// GetResultLogger function for getting log result
+func GetResultLogger() ResultLogger {
+	base := os.Getenv("STORAGE_DIR") + "/logs/"
+	return newFileResultLogger(base)
+}
+
+// StoreRequestResponse function for storing request and response into file
+// code string code of file
+// req []byte request json byte data
+// res []byte response json byte data
+func StoreRequestResponse(code string, req []byte, res []byte) string {
+	// set data to save/append into log
+	data := "REQUEST: " + string(req[:]) + " RESPONSE: " + string(res[:]) + "\n"
+
+	var fileLogger ResultLogger
+	fileLogger = GetResultLogger()
+
+	if fileLogger.LastError() != nil {
+		return ""
+	}
+
+	return fileLogger.RequestResponse(code, data)
+}
