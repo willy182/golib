cea05e6687a9c18e3a3f0177fcef3e176d5d1c17
jbernhardsch <julius.bernhard@gmail.com>
2017-09-25 22:33:04 +0700

    add helper file

---
+++ helper.go
@@ -0,0 +1,101 @@
+package golib
+
+import (
+	"errors"
+	"math/rand"
+	"regexp"
+	"time"
+)
+
+const (
+	// CHARS for setting short random string
+	CHARS = "abcdefghijklmnopqrstuvwxyz0123456789"
+	// this block is for validating URL format
+	email        string = "^(((([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+(\\.([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|\\.|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.)+(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.?$"
+	ip           string = `(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))`
+	urlSchema    string = `((ftp|tcp|udp|wss?|https?):\/\/)`
+	urlUsername  string = `(\S+(:\S*)?@)`
+	urlPath      string = `((\/|\?|#)[^\s]*)`
+	urlPort      string = `(:(\d{1,5}))`
+	urlIP        string = `([1-9]\d?|1\d\d|2[01]\d|22[0-3])(\.(1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.([0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))`
+	urlSubdomain string = `((www\.)|([a-zA-Z0-9]([-\.][-\._a-zA-Z0-9]+)*))`
+	url          string = `^` + urlSchema + `?` + urlUsername + `?` + `((` + urlIP + `|(\[` + ip + `\])|(([a-zA-Z0-9]([a-zA-Z0-9-_]+)?[a-zA-Z0-9]([-\.][a-zA-Z0-9]+)*)|(` + urlSubdomain + `?))?(([a-zA-Z\x{00a1}-\x{ffff}0-9]+-?-?)*[a-zA-Z\x{00a1}-\x{ffff}0-9]+)(?:\.([a-zA-Z\x{00a1}-\x{ffff}]{1,}))?))\.?` + urlPort + `?` + urlPath + `?$`
+	area         string = `^\+\d{1,5}$`
+	phone        string = `^\d{5,}$`
+	alphaNumeric string = `^[A-Za-z0-9][A-Za-z0-9 ._-]*$`
+)
+
+var (
+	// ErrBadFormatURL variable for error of url format
+	ErrBadFormatURL = errors.New("invalid url format")
+	// ErrBadFormatMail variable for error of email format
+	ErrBadFormatMail = errors.New("invalid email format")
+	// ErrBadFormatPhoneNumber variable for error of email format
+	ErrBadFormatPhoneNumber = errors.New("invalid phone format")
+
+	// emailRegexp regex for validate email
+	emailRegexp = regexp.MustCompile(email)
+	// urlRegexp regex for validate URL
+	urlRegexp = regexp.MustCompile(url)
+	// areaRegexp  regex for phone area number using +
+	areaRegexp = regexp.MustCompile(area)
+	// telpRegexp regex for phone number
+	phoneRegexp = regexp.MustCompile(phone)
+	// alphaNumericRegexp regex for validate uri
+	alphaNumericRegexp = regexp.MustCompile(alphaNumeric)
+)
+
+// RandomString function for creating randomized string
+// length int to set length of string
+func RandomString(length int) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	charsLength := len(CHARS)
+	result := make([]byte, length)
+	for i := 0; i < length; i++ {
+		result[i] = CHARS[rand.Intn(charsLength)]
+	}
+	return string(result)
+}
+
+// ValidateEmail function for validating email
+func ValidateEmail(email string) error {
+	if !emailRegexp.MatchString(email) {
+		return ErrBadFormatMail
+	}
+	return nil
+}
+
+// ValidateURL function for validating url
+func ValidateURL(str string) error {
+	if !urlRegexp.MatchString(str) {
+		return ErrBadFormatURL
+	}
+	return nil
+}
+
+// ValidatePhoneNumber function for validating phone number only
+func ValidatePhoneNumber(str string) error {
+	if !phoneRegexp.MatchString(str) {
+		return ErrBadFormatPhoneNumber
+	}
+	return nil
+}
+
+// ValidatePhoneAreaNumber function for validating area phone number
+func ValidatePhoneAreaNumber(str string) error {
+	if !areaRegexp.MatchString(str) {
+		return ErrBadFormatPhoneNumber
+	}
+	return nil
+}
+
+
+// ValidateAlphaNumeric function for checking alpha numeric string
+func ValidateAlphaNumeric(str string) bool {
+	if !alphaNumericRegexp.MatchString(str) {
+		return false
+	}
+	return true
+}
+
